07ff68cb3977167ae26dccd6ff351bd75c8be9f3
Dax Mahoney <daxmahoney@gmail.com>
2025-11-30 18:31:33 -0600

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,8 +1,12 @@
 from google.adk.agents import Agent
 from google.adk.models.google_llm import Gemini
-from google.adk.runners import InMemoryRunner
+
 from google.adk.tools import AgentTool, google_search
 from google.genai import types
+
+from google.adk.tools.preload_memory_tool import PreloadMemoryTool
+
+from typing import List
 
 retry_config=types.HttpRetryOptions(
     attempts=5,  # Maximum retry attempts
@@ -11,16 +15,35 @@ retry_config=types.HttpRetryOptions(
     http_status_codes=[429, 500, 503, 504], # Retry on these HTTP errors
 )
 
+async def auto_save_to_memory(callback_context):
+    """Automatically save session to memory after each agent turn."""
+    await callback_context._invocation_context.memory_service.add_session_to_memory(
+        callback_context._invocation_context.session
+    )
+print("✅ Callback created.")
+
+def count_papers(papers: List[str]):
+    """
+    This function counts the number of papers in a list of strings.
+    Args:
+      papers: A list of strings, where each string is a research paper.
+    Returns:
+      The number of papers in the list.
+    """
+    return len(papers)
+
 # Research Agent: Its job is to use the google_search tool and present findings.
 research_agent = Agent(
     name="ResearchAgent",
     model=Gemini(
-        model="gemini-3-pro-preview",
+        model="gemini-2.5-flash-lite",
         retry_options=retry_config
     ),
+    description="Searches for information using Google search",
     instruction="""You are a specialized research agent. Your only job is to use the
     google_search tool to find 2-3 pieces of relevant information on the topic of Adverse Childhood Experiences and present the findings with citations.""",
-    tools=[google_search],
+    tools=[google_search, PreloadMemoryTool()],
+    after_agent_callback=auto_save_to_memory,
     output_key="research_findings",  # The result of this agent will be stored in the session state with this key.
 )
 
@@ -30,9 +53,10 @@ print("✅ research_agent created.")
 summarizer_agent = Agent(
     name="SummarizerAgent",
     model=Gemini(
-        model="gemini-3-pro-preview",
+        model="gemini-2.5-flash-lite",
         retry_options=retry_config
     ),
+    description="Summarizes the google information",
     # The instruction is modified to request a bulleted list for a clear output format.
     instruction="""Read the provided research findings: {research_findings}
 Create a concise summary as a bulleted list with 3-5 key points and add the citations to the summary""",
@@ -45,18 +69,18 @@ print("✅ summarizer_agent created.")
 root_agent = Agent(
     name="ResearchCoordinator",
     model=Gemini(
-        model="gemini-3-pro-preview",
+        model="gemini-2.5-flash-lite",
         retry_options=retry_config
     ),
+    description="coordinates all of the agents",
     # This instruction tells the root agent HOW to use its tools (which are the other agents).
     instruction="""You are a research coordinator. Your goal is to answer the user's query by orchestrating a workflow.
 1. First, you MUST call the `ResearchAgent` tool to find relevant information on the topic provided by the user.
 2. Next, after receiving the research findings, you MUST call the `SummarizerAgent` tool to create a concise summary preserving the citation information.
-3. Next, present the final summary clearly to the user as your response along with the citations.
+3. Next, present the final summary clearly to the user as your response along with the citations and use the tool 'count_papers'.
 4. Finally, list all of the links to the papers mentioned""",
     # We wrap the sub-agents in `AgentTool` to make them callable tools for the root agent.
-    tools=[AgentTool(research_agent), AgentTool(summarizer_agent)],
+    tools=[AgentTool(research_agent), AgentTool(summarizer_agent), count_papers,],
+    after_agent_callback=auto_save_to_memory,
 )
-
 print("✅ root_agent created.")
-
